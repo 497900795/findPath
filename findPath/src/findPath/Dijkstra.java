0ddb8b26f5b93fced9eddc157a21acd993b89008
WZH <497900795@qq.com>
2018-02-20 20:35:25 +0800

    before debug

--- findPath/src/findPath/Dijkstra.java
+++ findPath/src/findPath/Dijkstra.java
@@ -1,5 +1,10 @@
 package findPath;
 import java.util.Scanner;
+import java.io.BufferedReader;  
+import java.io.File;  
+import java.io.FileInputStream;  
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 public class Dijkstra {
 	//常量
     private static int N = Integer.MAX_VALUE/2;
@@ -19,13 +24,13 @@ public class Dijkstra {
     //变量
     private static int start;
     private static int end;
-    private static boolean flag[] = new boolean [NUM];
-    private static int dist[]  = new int [NUM];
-    private static int  path[][] = new int [NUM][NUM];
+    private static boolean [] flag = new boolean [NUM];
+    private static int [] dist = new int [NUM];
+    private static int [][]  path = new int [NUM][NUM];
     //函数
     Dijkstra(){
-    	System.out.print("请输入开始结点:");
-    	Scanner input=new Scanner(System.in);	
+    	Scanner input=new Scanner(System.in);
+    	System.out.print("请输入开始结点:");		
     	start = input.nextInt();
     	System.out.print("请输入目标结点:");
     	end = input.nextInt();
@@ -44,7 +49,42 @@ public class Dijkstra {
     	dist[start] = 0;
     	flag[start] = true;
     }
-        
+    
+    Dijkstra(int inStart,int inEnd){
+    	start = inStart;
+    	end = inEnd;
+    	//path初始化
+    	for(int i = 0;i < NUM;i++){
+    		flag[i] = false;
+    		dist[i] = Graph[start][i];
+    		for(int j = 0;j < NUM;j++){
+    			path[i][j] = -1;
+    		}
+    		if(dist[i] != 0 && dist[i] != N){
+    			path[i][0] = start;
+    		}
+    	}
+    	dist[start] = 0;
+    	flag[start] = true;
+    }
+     
+    Dijkstra(int inStart){
+    	start = inStart;
+    	//path初始化
+    	for(int i = 0;i < NUM;i++){
+    		flag[i] = false;
+    		dist[i] = Graph[start][i];
+    		for(int j = 0;j < NUM;j++){
+    			path[i][j] = -1;
+    		}
+    		if(dist[i] != 0 && dist[i] != N){
+    			path[i][0] = start;
+    		}
+    	}
+    	dist[start] = 0;
+    	flag[start] = true;
+    }
+    
     public void dijkstra(){
     	int min,v=0;
     	int k;
@@ -84,5 +124,37 @@ public class Dijkstra {
     	}
     	System.out.print(end + "\n");
     	System.out.println("距离:" + dist[end]);
+    }
+    
+    public static int getNUM(){
+    	return NUM;
+    }
+    
+    public static int getDist(int point){
+    	return dist[point];
+    }
+    
+    public static int [] getPath(int point){
+    	return path[point];
+    }
+    
+    public static void setEnd(int inEnd){
+    	end = inEnd;
+    }
+    
+    public void resetGraph(int buliding,int floor) throws FileNotFoundException{
+    	//重置Graph
+    	Graph = null;
+    	String filePath = "./maps/" + buliding + "/" + floor + ".txt";//位置：maps/楼号/楼层.txt
+    	File file = new File(filePath);
+    	Scanner scanner = new Scanner(file);
+    	NUM = scanner.nextInt();
+    	Graph = new int [NUM][NUM];
+    	for(int i = 0;i < NUM;i++){
+    		for(int j = 0;j < NUM;j++){
+    			Graph[i][j] = scanner.nextInt();
+    		}
+    	}
+    	scanner.close();  	
     }
 }
